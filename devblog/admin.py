b001c4271d48fe02948ed673e7c09e4a8722069d
Luke Gibson <lukegibson101@gmail.com>
2022-05-23 22:26:29 +0000

    add summernote

--- devblog/admin.py
+++ devblog/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Blog
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Blog)
+
+@admin.register(Blog)
+class BlogAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
