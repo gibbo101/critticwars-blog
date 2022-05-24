1eb8e19ce0798c7611434de630fd471b11750eb3
Luke Gibson <lukegibson101@gmail.com>
2022-05-24 19:41:38 +0000

    set views logic for viewing blog posts and comments

--- devblog/views.py
+++ devblog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Blog
 
 class BlogList(generic.ListView):
@@ -7,3 +7,24 @@ class BlogList(generic.ListView):
     querysey = Blog.objects.order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 5
+
+
+class BlogDetail(View):
+
+    def get(self, slug, *args, **kwargs):
+        queryset = Blog.objects
+        blog = get_object_or_404(queryset, slug=slug)
+        comments = blog.comments.order_by('-created_on')
+        liked = False
+        if blog.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "blog.html",
+            {
+                "blog": blog,
+                "comments": comments,
+                "liked": liked
+            },
+        )
