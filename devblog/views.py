82c812f1fcbb6cfe94b6312e865dd8015b26d91e
Luke Gibson <lukegibson101@gmail.com>
2022-05-24 20:38:19 +0000

    set logic for creating a comment

--- devblog/views.py
+++ devblog/views.py
@@ -30,3 +30,32 @@ class BlogDetail(View):
                 "comment_form": CommentForm(),
             },
         )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Blog.objects
+        blog = get_object_or_404(queryset, slug=slug)
+        comments = blog.comments.order_by('-created_on')
+        liked = False
+        if blog.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance_name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.blog = blog
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "blog.html",
+            {
+                "blog": blog,
+                "comments": comments,
+                "liked": liked,
+                "comment_form": CommentForm(),
+            },
+        )
