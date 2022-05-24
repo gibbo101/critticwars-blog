8d7e0a022c537b686a452dc396cf8487211093d8
Luke Gibson <lukegibson101@gmail.com>
2022-05-24 21:32:17 +0000

    add likes url

--- devblog/urls.py
+++ devblog/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.BlogList.as_view(), name='home'),
     path('<slug:slug>/', views.BlogDetail.as_view(), name='blog'),
+    path('like/<slug:slug>', views.BlogLike.as_view(), name='blog_like'),
 ]
