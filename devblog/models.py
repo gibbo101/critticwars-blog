e63bb8a322aa337fb0d528a6d84258bfe8a5162d
Luke Gibson <lukegibson101@gmail.com>
2022-05-26 22:26:21 +0000

    add updated to comments

--- devblog/models.py
+++ devblog/models.py
@@ -32,6 +32,7 @@ class Comment(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now_add=True)
+    updated = models.BooleanField(default=False)
 
     class Meta:
         ordering = ['created_on']
