06b2a4f04cf37f6627cef6c8570d08842bb3d0c8
Luke Gibson <lukegibson101@gmail.com>
2022-05-27 23:41:38 +0000

    add form for account settings

--- devblog/forms.py
+++ devblog/forms.py
@@ -1,4 +1,4 @@
-from .models import Comment
+from .models import Comment, CwUsers
 from django import forms
 
 
@@ -6,3 +6,20 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('content',)
+
+
+class CritticWarsForm(forms.ModelForm):
+    class Meta:
+        model = CwUsers
+        fields = ('cw_id', 'cw_name',)
+
+        labels = {
+            'cw_name': 'CritticWars Name',
+            'cw_id': 'CritticWars ID'
+        }
+
+        error_messages = {
+            'cw_name': {
+                'max_length': "Name is too long.",
+            },
+        }
