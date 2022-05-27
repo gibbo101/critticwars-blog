94e028d1a8418a30086deb7d1d7f4bae67c345bf
Luke Gibson <lukegibson101@gmail.com>
2022-05-27 23:42:12 +0000

    set urls for settings and delete account

--- devblog/urls.py
+++ devblog/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.BlogList.as_view(), name='home'),
+    path('settings/', views.Settings.as_view(), name='settings'),
+    path('delete/', views.Delete.as_view(), name='delete'),
     path('<slug:slug>/', views.BlogDetail.as_view(), name='blog'),
     path('like/<slug:slug>/', views.BlogLike.as_view(), name='blog_like'),
     path('<slug:slug>/edit_comment/<id>/', views.CommentEdit.as_view(), name='edit'),
