0f3188a74c7ab71bd579a5a68d5bb70dab718050
Luke Gibson <lukegibson101@gmail.com>
2022-05-27 16:44:20 +0000

    amend urls

--- devblog/urls.py
+++ devblog/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.BlogList.as_view(), name='home'),
     path('<slug:slug>/', views.BlogDetail.as_view(), name='blog'),
-    path('like/<slug:slug>', views.BlogLike.as_view(), name='blog_like'),
-    path('<slug:slug>/edit_comment/<id>', views.CommentEdit.as_view(), name='edit'),
-    path('<slug:slug>/delete_comment/<id>', views.CommentDelete.as_view(), name='delete'),
+    path('like/<slug:slug>/', views.BlogLike.as_view(), name='blog_like'),
+    path('<slug:slug>/edit_comment/<id>/', views.CommentEdit.as_view(), name='edit'),
+    path('<slug:slug>/delete_comment/<id>/', views.CommentDelete.as_view(), name='delete'),
 ]
