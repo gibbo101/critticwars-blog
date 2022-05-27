29cbceb2ca3131121d2445884eda77a9b0add8ff
Luke Gibson <lukegibson101@gmail.com>
2022-05-27 23:42:42 +0000

    update view to account settings and delete account

--- devblog/views.py
+++ devblog/views.py
@@ -2,8 +2,8 @@ from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
-from .models import Blog, Comment
-from .forms import CommentForm
+from .models import Blog, Comment, CwUsers, User
+from .forms import CommentForm, CritticWarsForm
 import datetime
 
 
@@ -30,6 +30,7 @@ class BlogDetail(View):
             {
                 "blog": blog,
                 "comments": comments,
+                "cw_users": cw_users,
                 "liked": liked,
                 "comment_form": CommentForm(),
             },
@@ -37,6 +38,8 @@ class BlogDetail(View):
 
     def post(self, request, slug, *args, **kwargs):
         queryset = Blog.objects
+        users = User.objects
+        user = get_object_or_404(users, id=self.request.user.id)
         blog = get_object_or_404(queryset, slug=slug)
         comments = blog.comments.order_by('-created_on')
         liked = False
@@ -47,6 +50,7 @@ class BlogDetail(View):
 
         if comment_form.is_valid():
             comment_form.instance.name = request.user.username
+            comment_form.instance.name_id = user
             comment = comment_form.save(commit=False)
             comment.blog = blog
             comment.save()
@@ -130,3 +134,62 @@ class CommentDelete(View):
             return HttpResponseRedirect(reverse('blog', args=[slug]), messages.add_message(request, messages.ERROR, 'Comment Deleted'))
         else:
             return HttpResponseRedirect(reverse('blog', args=[slug]), messages.add_message(request, messages.ERROR, 'Error'))
+
+class Delete(View):
+
+    def get(self, request):
+        queryset = User.objects.filter(id=self.request.user.id)
+        delete = get_object_or_404(queryset, id=self.request.user.id)
+
+        return render(
+            request,
+            "delete.html",
+            {
+                "delete": delete,
+            },
+        )
+
+    def post(self, request):
+        queryset = User.objects.filter(id=self.request.user.id)
+        delete = get_object_or_404(queryset, id=self.request.user.id)
+
+        if delete:
+            delete.delete()
+            return HttpResponseRedirect('/')
+        else:
+            return HttpResponseRedirect('home'), messages.add_message(request, messages.ERROR, 'Error')
+
+
+class Settings(View):
+
+    def get(self, request, *args, **kwargs):
+        queryset = CwUsers.objects.filter(user_id=self.request.user.id)
+        users = User.objects
+        user = get_object_or_404(users, id=self.request.user.id)
+        if not queryset:
+            user_settings = CwUsers(cw_name=self.request.user.username, cw_id=0, user_id=user)
+            user_settings.save()
+
+        user_settings = get_object_or_404(queryset, user_id=self.request.user.id)
+
+        return render(
+            request,
+            "settings.html",
+            {
+                "user_settings": user_settings,
+                "user_form": CritticWarsForm(instance=user_settings),
+            },
+        )
+
+    def post(self, request, *args, **kwargs):
+        queryset = CwUsers.objects.filter(user_id=self.request.user.id)
+        user_settings = get_object_or_404(queryset, user_id=self.request.user.id)
+        user_form = CritticWarsForm(data=request.POST, instance=user_settings)
+
+        if user_form.is_valid():
+            user = user_form.save(commit=False)
+            user.save()
+        else:
+            user_form = CritticWarsForm()
+
+        return HttpResponseRedirect(reverse('home'), messages.add_message(request, messages.SUCCESS, 'User Settings Edited'))
