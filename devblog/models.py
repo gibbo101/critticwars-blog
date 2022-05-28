d9529c909c8ebc227cbcc029bc82506ecbaf7ff2
Luke Gibson <lukegibson101@gmail.com>
2022-05-28 11:06:32 +0000

    remove image field from Blog

--- devblog/models.py
+++ devblog/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
 
 
 class Blog(models.Model):
@@ -8,7 +7,6 @@ class Blog(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     content = models.TextField()
-    image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now=True)
     updated_on = models.DateTimeField(auto_now=True)
