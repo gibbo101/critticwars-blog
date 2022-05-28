34f1851293ea975a04ef56ec048356a930d4f598
Luke Gibson <lukegibson101@gmail.com>
2022-05-28 11:09:44 +0000

    add alerts on delete

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,11 @@ setTimeout(function() {
     let alert = new bootstrap.Alert(messages);
     alert.close();
 }, 2000);
+
+function deleteAccount() {
+    alert("This action can't be undone! Do you really want to delete your account?");
+}
+
+function deleteComment() {
+    alert("This action can't be undone! Do you really want to delete this comment?");
+}
